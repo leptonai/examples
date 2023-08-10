c650329509f374ea795adf253b91527e6b7a7f46
Yangqing Jia <me@daggerfs.com>
2023-08-09 20:37:30 -0700

    whisper example

---
+++ advanced/whisper-jax/README.md
@@ -0,0 +1,142 @@
+# Whisper 
+
+This example demonstrates how to run optimized Whisper model on Lepton, and also how to integrate it with other application frameworks, in this case Slack.
+
+[whisper-jax](https://github.com/sanchit-gandhi/whisper-jax.git) is a JAX (optimized) port of the openai whisper model. It chunks audio data into segments and then performs batch inference to gain speedup.
+
+
+## Note on custom environment
+
+Usually, when you run different AI models, they require specific dependencies that sometimes conflict with each other. This is particularly true in the whisper case - from `requirements.txt`, you may notice that there are quite a bit of specific version requirements.
+
+This is where having a separate service like Lepton becomes super useful: we can create a python environment (using e.g. conda or virtualenv), installed the required dependencies, run the photon as a web service, and then in the regular python environment, simply call the web service as if we were using a regular python function. Comparing to some apparent choices:
+- unlike a single python environment, we don't need to resolve version conflicts of different algorithms;
+- unlike packing everything in a separate opaque container image, we are much more lightweighted: only a python environment and dependencies are needed.
+
+Here we provide a combination of jax + jaxlib + cuda/cudnn pip versions that can work together inside Lepton's default image, so you can pull it up with ease.
+
+## Running with a custom environment.
+
+We recommend you use conda or virtualenv to start a whisper-specific environment. For example, if you use conda, it's easy to do:
+
+```shell
+# pick a python version of your favorite
+conda create -n whisper python=3.10 
+conda activate whisper
+```
+
+After that, install lepton [per the installation instruction](https://www.lepton.ai/docs/overview/quickstart#1-installation), and install the required dependencies of this demo via:
+```shell
+pip install -r requirements.txt
+```
+
+Note that `torch` sometimes has a dependency conflict with the current jax gpu libraries. If you installed torch after the above pip install and encountered errors, re-run the above installation instruction.
+
+After this, you can launch whisper like:
+```shell
+# Set the model id, or omit it to use the default large-v2 model.
+# Note that the large-v2 model may need ~10GB GPU memory to run.
+export WHISPER_MODEL_ID="openai/whisper-medium"
+python whisper-jax.py
+```
+
+It will download the paramaters and start the server. After that, use the regular python client to access the model:
+```python
+from leptonai.client import Client, local
+c = Client(local())
+```
+
+and invoke transcription or translation as follows:
+```python
+>> c.run(inputs="assets/test_japanese.wav")
+'私たちはAIの株式会社であります'
+>> c.run(inputs="assets/test_japanese.wav", task="translate")
+'We are an AI company.'
+```
+
+## Running a slack translation bot
+
+The whisper-jax example also demonstrates how to use Slack bot to trigger inference. To use this feature, you need to create a slack app, and set the following environment variables:
+- `SLACK_VERIFICATION_TOKEN`: The verification token of your Slack app
+- `SLACK_BOT_TOKEN`: The bot token of your Slack app
+
+Let's go through the process one by one.
+
+### Creating a slack app
+
+First you will need to create a slack app. Go to [https://api.slack.com/apps](https://api.slack.com/apps), and click "Create an App". Choose "From scratch", and select the app name and workspace you want to add the app to, like:
+<img src="assets/create_slack_app.png" width=400>
+
+In "OAuth & Permisions", add the following permissions to the app:
+- app_methions:read
+- chat:write
+- files:read
+
+Which looks like the follows:
+
+<img src="assets/permissions.png" width=400>
+
+And install it to your workspace. After that, you should be able to see the Bot User OAuth Token like the following:
+
+<img src="assets/bot_token.png" width=400>
+
+Locate the verification token in the app's "Basic Information"-"App Credentials" section. Then, launch the service as follows:
+```shell
+export SLACK_BOT_TOKEN="xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+export SLACK_VERIFICATION_TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+python whisper-jax.py
+```
+
+You will need a public address for slack to connect. For example, if the whisper-jax bot tells you `Uvicorn running on http://0.0.0.0:8080`, and your public IP is `1.2.3.4`, you can go to the "Event Subscriptions" page of the slack app, and enable events like the following (make sure that the request URL says "Verified"):
+![](assets/event_subscription.png)
+You should also be able to see the verification request from the logging info like
+```
+2023-08-09 16:35:16,119 - INFO:     34.203.249.192:47706 - "POST /slack HTTP/1.1" 200 OK
+```
+
+Now, in the "Event Subscriptions"-"Subscribe to bot events" page, add the bot user event "file_shared", and save. If everything goes well, you should be able to see the bot responding to voice inputs as follows:
+
+<img src="assets/whisper_result.jpg" width=400>
+
+The whisper model is fairly versatile - in this case we are showing the medium sized model, and it is already able to recognize multiple languages with a fairly good accuracy.
+
+## Running with Lepton
+
+The above example runs on the local machine. If your machine does not have a public facing IP, or more commonly, you want a stable server environment to host your model - then running on the Lepton cloud platform is the best option. To run it on Lepton, you can simply create a photon and push it to the cloud:
+
+```shell
+lep login
+lep photon create -n whisper -m whisper-jax.py
+lep photon push -n whisper
+# An A10 machine is usually big enough to run the large-v2 model.
+lep photon run -n whisper
+  --resource-shape gpu.a10 \
+  --public \
+  --env WHISPER_MODEL_ID="openai/whisper-large-v2" \
+  --env SLACK_BOT_TOKEN="xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxx" \
+  --env SLACK_VERIFICATION_TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+```
+
+Note that because we want Slack to be able to call the API, we made it a public deployment. After that, you can use the `lep deployment status` to obtain the public address of the photon, and use the same slack app to connect to it:
+```shell
+>> lep deployment status -n whisper
+Created at: 2023-08-09 20:24:48
+Photon ID:  whisper-6t01ptsf
+State:      Running
+Endpoint:   https://latest-whisper.cloud.lepton.ai
+Is Public:  Yes
+Replicas List:
+┏━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━┳━━━━━━━━━┓
+┃ replica id              ┃ status ┃ message ┃
+┡━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━╇━━━━━━━━━┩
+│ whisper-f9dbd6757-l8hms │ Ready  │ (empty) │
+└─────────────────────────┴────────┴─────────┘
+```
+When you obtain the endpoint URL above, simply change the event subscription URL to the new endpoint, such as in this case, `https://latest-whisper.cloud.lepton.ai/slack`, and you should be able to use the same slack app.
+
+Unlike local deployment, running on the Lepton cloud platform comes with a series of advantages, especially in the whisper case:
+- You do not need to worry about reproducible software environment. The photon is guaranteed to run on the same environment as you created it.
+- Scaling is easier - you can simply increase the number of replicas if you need more capacity.
+- Automatic fault tolerance - if the photon crashes, it will be automatically restarted.
+
+Happy building!
--- advanced/whisper-jax/whisper-jax.py
+++ advanced/whisper-jax/whisper-jax.py
@@ -9,6 +9,21 @@ Installing JAX is a bit tricky, so here we provide a combination of
 jax + jaxlib + cuda/cudnn pip versions that can work together inside
 Lepton's default image.
 
+Whisper has a set of model ids that you can use. This is specified by an
+environment variable "WHISPER_MODEL_ID". By default, it uses "openai/whisper-large-v2".
+The list of available models are "openai/whisper-{size}" where size can be one of
+the following:
+    tiny, base, small, medium, large, large-v2
+See https://github.com/sanchit-gandhi/whisper-jax for more details.
+
+Optionally, you can also set the environment variable "BATCH_SIZE" to
+change the batch size of the inference. By default, it is 4.
+
+In addition, this example also demonstrates how to use Slack bot to
+trigger inference. To use this feature, you need to set the following
+environment variables:
+- `SLACK_VERIFICATION_TOKEN`: The verification token of your Slack app
+- `SLACK_BOT_TOKEN`: The bot token of your Slack app
 """
 
 from datetime import datetime, timedelta
@@ -23,6 +38,11 @@ from leptonai.photon import Photon, HTTPException
 
 
 class Whisper(Photon):
+    """
+    A photon implementatio
+    """
+
+    # note:
     requirement_dependency = [
         "git+https://github.com/sanchit-gandhi/whisper-jax.git@0d3bc54",
         "cached_property",
@@ -33,29 +53,46 @@ class Whisper(Photon):
         "slack_sdk",
     ]
 
+    # note: system_dependency specifies what should be installed via `apt install`
     system_dependency = [
         "ffmpeg",
     ]
 
     def init(self):
+        # Implementation note: strictly speaking, this is not recommended by Python
+        # as all imports should be places on the top of the file. However, this shows
+        # a small trick when a local installation isn't really possible, such as
+        # installing all the jax and cuda dependencies on a mac machine. We can defer
+        # the import inside the actual Photon class.
+        # Of course, this makes the debugging duty to the remote execution time, and
+        # is going to be a bit harder. This is a conscious tradeoff between development
+        # speed and debugging speed.
+        logger.info("Initializing Whisper model. This might take a while...")
         from whisper_jax import FlaxWhisperPipline
         import jax.numpy as jnp
 
-        model_id = os.environ.get("MODEL_ID", "openai/whisper-large-v2")
+        model_id = os.environ.get("WHISPER_MODEL_ID", "openai/whisper-large-v2")
         batch_size = os.environ.get("BATCH_SIZE", 4)
+        logger.info(f"Using model id: {model_id} and batch size: {batch_size}")
         self.pipeline = FlaxWhisperPipline(
             model_id, dtype=jnp.float16, batch_size=batch_size
         )
-
+        logger.info("Initialized Whisper model.")
+        logger.info("Initializing slack bot...")
         self._init_slack_bot()
 
     def _init_slack_bot(self):
+        """
+        Initializes the slack bot client.
+        """
         from slack_sdk import WebClient as SlackClient
 
         self._verification_token = os.environ.get("SLACK_VERIFICATION_TOKEN", None)
         self._slack_bot_token = os.environ.get("SLACK_BOT_TOKEN", None)
         if self._slack_bot_token:
             self._slack_bot_client = SlackClient(token=self._slack_bot_token)
+        else:
+            logger.warning("Slack bot token not configured. Slack bot will not work.")
         self._processed_slack_tasks = {}
 
     @Photon.handler(
@@ -66,10 +103,25 @@ class Whisper(Photon):
             )
         },
     )
-    def run(self, inputs: str, task: Optional[str] = None):
+    def run(self, inputs: str, task: Optional[str] = None) -> str:
+        """
+        Transcribe or translate an audio input file to a text transcription.
+
+        Args:
+            inputs: the filename or url of the audio file.
+            task (optional): either `"transcribe"` or `"translate"`. Defaults to `"transcribe"`.
+
+        Returns:
+            text: the transcription of the audio file.
+        """
         return self.pipeline(inputs, task=task)["text"]
 
-    def _slack_process_task(self, channel: str, thread_ts: str, url: str):
+    async def _slack_process_task(
+        self, channel: str, thread_ts: Optional[str], url: str
+    ):
+        """
+        Internal method to process a slack task. This is called by the `slack` handler.
+        """
         last_processed_time = self._processed_slack_tasks.get((channel, url))
         if last_processed_time and datetime.now() - last_processed_time < timedelta(
             seconds=20
@@ -95,15 +147,31 @@ class Whisper(Photon):
             f.seek(0)
             logger.info(f"Saved audio file to: {f.name}")
             logger.info(f"Running inference on audio file: {f.name}")
-            text = self.run(f.name)
+            try:
+                text = self.run(f.name)
+            except Exception as e:
+                logger.error(f"Failed to run inference on audio file: {f.name}")
+                return
             logger.info(f"Finished inference on audio file: {f.name}")
         self._slack_bot_client.chat_postMessage(
             channel=channel,
             thread_ts=thread_ts,
             text=text,
         )
+        if len(self._processed_slack_tasks) > 100:
+            # clean up task histories that are too old.
+            self._processed_slack_tasks = {
+                k: v
+                for k, v in self._processed_slack_tasks.items()
+                if datetime.now() - v < timedelta(seconds=20)
+            }
         self._processed_slack_tasks[(channel, url)] = datetime.now()
 
+    # This is a handler that receives slack events. It is triggered by the
+    # slack server side - see the slack event api for details:
+    # https://api.slack.com/apis/connections/events-api#receiving-events
+    # and more specs here:
+    # https://github.com/slackapi/slack-api-specs
     @Photon.handler
     def slack(
         self,
@@ -111,30 +179,50 @@ class Whisper(Photon):
         type: str,
         event: Optional[Dict[str, Any]] = None,
         challenge: Optional[str] = None,
-        **extra,
+        **exttra,
     ) -> str:
+        # Checks if the slack bot is configured. Note that we are still using the
+        # now deprecated verification token, an supporting the new signing secrets
+        # is a todo item.
         if not self._verification_token or not self._slack_bot_token:
-            raise HTTPException(401, "Slack bot not configured")
-
+            raise HTTPException(401, "Slack bot not configured.")
+        # If token is not correct, we return 401.
         if token != self._verification_token:
-            raise HTTPException(401, "Invalid token")
-
+            raise HTTPException(401, "Invalid token.")
+        # We will respond to the challenge request if it is a url_verification event,
+        # so that slack can verify our endpoint.
         if type == "url_verification":
-            return challenge
+            if challenge:
+                return challenge
+            else:
+                raise HTTPException(400, "Missing challenge")
 
+        # If event is not present, we will ignore it.
+        if not event:
+            raise HTTPException(400, "Missing event type")
+
+        # Actually handle the slack event. We will only handle file_shared events.
         event_type = event["type"]
         logger.info(f"Received slack event: {event_type}")
-
         if event_type == "file_shared":
             channel = event["channel_id"]
             thread_ts = event.get("thread_ts")
             file_id = event["file_id"]
             file_info = self._slack_bot_client.files_info(file=file_id)
             if not file_info["ok"]:
-                raise HTTPException(500, "Failed to get file info")
-            self._slack_process_task(
-                channel, thread_ts, file_info["file"]["url_private"]
+                raise HTTPException(500, "Failed to get file info from slack")
+            self.add_background_task(
+                self._slack_process_task,
+                channel,
+                thread_ts,
+                file_info["file"]["url_private"],
             )
             return "ok"
+        else:
+            logger.info(f"Ignored slack event type: {event_type}")
+            return "ok"
 
-        logger.info(f"Ignored slack event type: {event_type}")
+
+if __name__ == "__main__":
+    w = Whisper()
+    w.launch()

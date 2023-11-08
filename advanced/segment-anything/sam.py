leptonai/examples

commit 317b57549a6777600f5e3617c748d1dce71d0ad6

convert image mode to RGB for sam generator (#52)

advanced/segment-anything/sam.py
@@ -144,7 +144,7 @@ class SAM(Photon):
         # urllib to do this. Note that this is just an example - you can use any method
         # to download the image.
         try:
-            raw_img = np.asarray(Image.open(io.BytesIO(urlopen(url).read())))
+            raw_img = np.asarray(Image.open(io.BytesIO(urlopen(url).read())).convert("RGB"))
         except Exception as e:
             # HTTPException is a special exception that will be translated to a proper
             # http error by fastAPI and return to the user side. In this case, we will
@@ -203,7 +203,7 @@ class SAM(Photon):
         masks.
         """
         try:
-            raw_img = np.asarray(Image.open(io.BytesIO(urlopen(url).read())))
+            raw_img = np.asarray(Image.open(io.BytesIO(urlopen(url).read())).convert("RGB"))
         except Exception as e:
             raise HTTPException(
                 status_code=400,

commit c182921ece23bd1cf95402e14b202496a5e3d1bd

Fix lint error introduced in #52

advanced/segment-anything/sam.py
@@ -144,7 +144,9 @@ class SAM(Photon):
         # urllib to do this. Note that this is just an example - you can use any method
         # to download the image.
         try:
-            raw_img = np.asarray(Image.open(io.BytesIO(urlopen(url).read())).convert("RGB"))
+            raw_img = np.asarray(
+                Image.open(io.BytesIO(urlopen(url).read())).convert("RGB")
+            )
         except Exception as e:
             # HTTPException is a special exception that will be translated to a proper
             # http error by fastAPI and return to the user side. In this case, we will
@@ -203,7 +205,9 @@ class SAM(Photon):
         masks.
         """
         try:
-            raw_img = np.asarray(Image.open(io.BytesIO(urlopen(url).read())).convert("RGB"))
+            raw_img = np.asarray(
+                Image.open(io.BytesIO(urlopen(url).read())).convert("RGB")
+            )
         except Exception as e:
             raise HTTPException(
                 status_code=400,
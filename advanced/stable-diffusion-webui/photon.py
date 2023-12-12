91b190ded1dcb9867b9f8d8f2bffc6466e383575
Junjie Bai <jbai@lepton.ai>
2023-12-12 00:29:08 +0000

    fix lint

--- advanced/stable-diffusion-webui/photon.py
+++ advanced/stable-diffusion-webui/photon.py
@@ -11,7 +11,8 @@ class WebUI(Photon):
         (
             "apt-get update && apt-get install -y wget libgoogle-perftools-dev && wget"
             f" -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/{webui_version}/webui.sh"
-            " && chmod +x ./webui.sh && ACCELERATE=True ./webui.sh -f --listen --port 8080"
+            " && chmod +x ./webui.sh && ACCELERATE=True ./webui.sh -f --listen --port"
+            " 8080"
         ),
     ]
     deployment_template = {

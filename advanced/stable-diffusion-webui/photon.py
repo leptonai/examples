leptonai/examples

commit 19a3ef5bf95057e301e4d1912c9b8df8f5483dfa

Add Stable Diffusion web UI example (#63)
* Add stable-diffusion-webui example

* Add Stable Diffusion web UI example

* fix lint

advanced/stable-diffusion-webui/photon.py
@@ -1,0 +1,20 @@
+import os
+
+from leptonai.photon import Photon
+
+
+class WebUI(Photon):
+    webui_version = "1.6.0"
+    cmd = [
+        "bash",
+        "-c",
+        (
+            "apt-get update && apt-get install -y wget libgoogle-perftools-dev && wget"
+            f" -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/{webui_version}/webui.sh"
+            " && chmod +x ./webui.sh && ACCELERATE=True ./webui.sh -f --listen --port"
+            " 8080"
+        ),
+    ]
+    deployment_template = {
+        "resource_shape": "gpu.a10",
+    }

commit 35f8e710dc9f66fc4e991addc0f912e27998ad3c

fix lint error introduced in #63

advanced/stable-diffusion-webui/photon.py
@@ -1,5 +1,3 @@
-import os
-
 from leptonai.photon import Photon
 
 